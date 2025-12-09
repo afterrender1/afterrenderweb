6f295cac7535a4a332be8ea576fa3cc0eef2a81d
afterrender1 <afterrender1@gmail.com>
2025-12-09 15:00:47 +0500

    analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
       
       >
+        <Analytics />
         {children}
       </body>
     </html>
