3ea519dcf7daca5fe1f33c72f9423771d513a056
afterrender1 <afterrender1@gmail.com>
2026-09-15 08:47:25 -0700

    Update CollectionPortfolio.jsx

--- components/CollectionPortfolio.jsx
+++ components/CollectionPortfolio.jsx
@@ -40,8 +40,6 @@ export const videosCategories = [
   "Shorts form edits",
   "Talking Heads",
   "RealEstate",
-  // "Podcast Intro",
-  // "Digital Course VSL",
 ];
 
 export const graphicsItems = [
@@ -469,26 +467,6 @@ export const videosItems = [
     isVertical: false,
   },
   {
-    id: "montage-1",
-    title: "Montage #1",
-    category: "Montage style",
-    videoUrl:
-      "https://res.cloudinary.com/dlurrugno/video/upload/v1789477840/Han_reel_1_ik1u7i.mp4",
-    posterUrl: "/images/montageposters/montage-poster1.png",
-    aspect: "aspect-[9/16]",
-    isVertical: true,
-  },
-  {
-    id: "montage-2",
-    title: "Montage #2",
-    category: "Montage style",
-    videoUrl:
-      "https://res.cloudinary.com/dlurrugno/video/upload/v1789478071/Diceman_drive_keykmv.mp4",
-    posterUrl: "/images/montageposters/montage-poster2.png",
-    aspect: "aspect-[9/16]",
-    isVertical: true,
-  },
-  {
     id: "montage-3",
     title: "Montage #3",
     category: "Montage style",
@@ -499,9 +477,29 @@ export const videosItems = [
     isVertical: true,
   },
   {
-    id: "montage-4",
-    title: "Montage #4",
-    category: "Montage style",
+    id: "realestate-1",
+    title: "RealEstate #1",
+    category: "RealEstate",
+    videoUrl:
+      "https://res.cloudinary.com/dlurrugno/video/upload/v1789477840/Han_reel_1_ik1u7i.mp4",
+    posterUrl: "/images/montageposters/montage-poster1.png",
+    aspect: "aspect-[9/16]",
+    isVertical: true,
+  },
+  {
+    id: "realestate-2",
+    title: "RealEstate #2",
+    category: "RealEstate",
+    videoUrl:
+      "https://res.cloudinary.com/dlurrugno/video/upload/v1789478071/Diceman_drive_keykmv.mp4",
+    posterUrl: "/images/montageposters/montage-poster2.png",
+    aspect: "aspect-[9/16]",
+    isVertical: true,
+  },
+  {
+    id: "realestate-3",
+    title: "RealEstate #3",
+    category: "RealEstate",
     videoUrl:
       "https://res.cloudinary.com/dlurrugno/video/upload/v1789481191/Reel_01_p64dct.mp4",
     posterUrl: "/images/montageposters/montage-poster4.png",
@@ -509,9 +507,9 @@ export const videosItems = [
     isVertical: true,
   },
   {
-    id: "montage-5",
-    title: "Montage #5",
-    category: "Montage style",
+    id: "realestate-4",
+    title: "RealEstate #4",
+    category: "RealEstate",
     videoUrl:
       "https://res.cloudinary.com/dlurrugno/video/upload/v1789485708/Reel_09_wszdtq.mp4",
     posterUrl: "/images/montageposters/montage-poster5.png",
